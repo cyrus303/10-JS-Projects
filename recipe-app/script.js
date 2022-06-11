ce215cb937f5cc9397f2fef50d7407dac54c75b1
cyrus303 <sachinsunny303@gmail.com>
2022-06-11 08:06:54 +0100

    added local storage but a bug has been discoved will reading the data

--- recipe-app/script.js
+++ recipe-app/script.js
@@ -1,6 +1,8 @@
 const meals = document.getElementById('meals');
+const favouriteContainer = document.getElementById('fav-meals');
 
 getRandomMeal();
+fetchFavMeals();
 
 async function getRandomMeal() {
   const response = await fetch(
@@ -13,19 +15,24 @@ async function getRandomMeal() {
   addMeal(randomMeal, true);
 }
 
-async function getMealById() {
-  const meal = await fetch(
-    ' www.themealdb.com/api/json/v1/1/lookup.php?i=' + id
+async function getMealById(mealId) {
+  const resp = await fetch(
+    'https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + mealId
   );
+  const respData = await resp.json();
+  // console.log(respData);
+  const meal = respData.meals[0];
+  return meal;
 }
 
 async function getMealsBySearch(item) {
   const meals = await fetch(
-    ' www.themealdb.com/api/json/v1/1/search.php?s=' + item
+    'https://www.themealdb.com/api/json/v1/1/search.php?s=' + item
   );
 }
 
 function addMeal(mealData, random = false) {
+  // console.log(mealData);
   const meal = document.createElement('div');
   meal.classList.add('meal');
   meal.innerHTML = `<div class="meal-header">
@@ -40,12 +47,68 @@ function addMeal(mealData, random = false) {
     </div>`;
 
   meal.querySelector('.meal-body .fav-btn').addEventListener('click', (e) => {
-    e.target.classList.toggle('active');
-  });
+    if (e.target.classList.contains('active')) {
+      removeMealLS(mealData.idMeal);
+      e.target.classList.remove('active');
+    } else {
+      addMealsLS(mealData.idMeal);
+      e.target.classList.add('active');
+    }
 
+    favouriteContainer.innerHTML = '';
+    fetchFavMeals();
+  });
   meals.appendChild(meal);
 }
 
-function addMealsToLS(meal) {}
+function removeMealLS(mealId) {
+  const mealIds = getMealsLS();
+  localStorage.setItem(
+    'mealIds',
+    JSON.stringify([mealIds.filter((id) => id !== mealId)])
+  );
+}
 
-function getMealsFromLS() {}
+function addMealsLS(mealId) {
+  const mealIds = getMealsLS();
+  let totalMeals = [...mealIds, mealId];
+  localStorage.setItem('mealIds', JSON.stringify(totalMeals));
+}
+
+// function getMealsLS() {
+//   const mealIds = JSON.parse(localStorage.getItem('mealIds'));
+//   console.log(mealIds);
+//   // return mealIds;
+//   return mealIds === null ? 0 : mealIds;
+// }
+
+function getMealsLS() {
+  const mealIds = JSON.parse(localStorage.getItem('mealIds'));
+  console.log(mealIds);
+  return mealIds === null ? [] : mealIds;
+  // todo: bug in returing value, if its null, the local storage breaks after few itteration
+}
+
+async function fetchFavMeals() {
+  const mealIds = getMealsLS();
+  // console.log(mealIds);
+  const meals = [];
+  for (let i = 0; i < mealIds.length; i++) {
+    const mealId = mealIds[i];
+    meal = await getMealById(mealId);
+    addMealToFav(meal);
+  }
+  // add them to the screen
+}
+
+function addMealToFav(mealData) {
+  // console.log(mealData);
+  const favMeal = document.createElement('li');
+
+  favMeal.innerHTML = `<li><img src="${mealData.strMealThumb}" alt="${mealData.strMeal}"><span>${mealData.strMeal}</span></li>`;
+
+  favouriteContainer.appendChild(favMeal);
+}
+
+// !stopped at 2:27:33 of the video
+// !https://www.youtube.com/watch?v=dtKciwk_si4

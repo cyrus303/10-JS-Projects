a2ec85e9677ab63a1e4f9feb1405593225d55f62
cyrus303 <sachinsunny303@gmail.com>
2022-06-13 20:16:09 +0100

    movie app completed with search functionality

--- movie-app/script.js
+++ movie-app/script.js
@@ -6,33 +6,50 @@ const APIURL =
 
 const IMGPATH = 'https://image.tmdb.org/t/p/w1280';
 
-const main = document.querySelector('main');
+const SEARCHAPI =
+  'https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=';
 
-async function getMovies() {
-  const resp = await fetch(APIURL);
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
+// initally get movies to populate the screen
+getMovies(APIURL);
+
+async function getMovies(url) {
+  const resp = await fetch(url);
   const respData = await resp.json();
 
-  console.log(respData);
+  console.log(respData.results);
+  showMovies(respData.results);
+}
 
-  respData.results.forEach((movie) => {
-    const { poster_path, title, vote_average } = movie;
-    const movieEl = document.createElement('div');
-    movieEl.classList.add('movie');
+function showMovies(movies) {
+  //clear main
+  main.innerHTML = '';
+  movies.forEach((movie) => {
+    const { poster_path, title, vote_average, backdrop_path, overview } = movie;
+    if (poster_path || backdrop_path) {
+      const movieEl = document.createElement('div');
+      movieEl.classList.add('movie');
 
-    movieEl.innerHTML = `
-    <img
-      src="${IMGPATH + poster_path}"
-      alt="movie poster"
-    />
-    <div class="movie-info">
-      <h3>${title}<h3>
-      <span class = '${getClassByRate(vote_average)}'>${vote_average}</span>
-    </div>`;
+      movieEl.innerHTML = `
+      <img
+        src="${IMGPATH + `${poster_path ? poster_path : backdrop_path}`}"
+        alt="movie poster"
+      />
+      <div class="movie-info">
+        <h3>${title}<h3>
+        <span class = '${getClassByRate(vote_average)}'>${vote_average}</span>
+      </div>
+      
+      <div class="overview">
+      <h3>Overview</h3>${overview}</div>
+      `;
 
-    main.appendChild(movieEl);
+      main.appendChild(movieEl);
+    }
   });
-
-  return respData;
 }
 
 function getClassByRate(vote) {
@@ -45,4 +62,13 @@ function getClassByRate(vote) {
   }
 }
 
-getMovies();
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value;
+
+  if (searchTerm) {
+    getMovies(SEARCHAPI + searchTerm);
+    search.value = '';
+  }
+});

385ce4a91a3f9b3f91ef2896c58018e9124189d4
cyrus303 <sachinsunny303@gmail.com>
2022-06-17 07:17:24 +0100

    password generator UI completed

---
+++ password-generator/script.js
@@ -0,0 +1,15 @@
+const copyEl = document.getElementById('copy');
+const pwEl = document.getElementById('pw');
+const lenEl = document.getElementById('len');
+const upperEl = document.getElementById('upper');
+const lowerEl = document.getElementById('lower');
+const numberEl = document.getElementById('number');
+const symbolEl = document.getElementById('symbol');
+const generateEl = document.getElementById('generate');
+
+const upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVWZYX';
+const lowerLetters = 'abcdefghijklmnopqerstuvwxyz';
+const numbers = '0123456789';
+const symbols = '!@#$%^&*()_+=';
+
+function 
